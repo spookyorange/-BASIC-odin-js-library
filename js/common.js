54bde5fb830cec7906402a5bda4b9a64680217fc
spookyorange <korkmazcoltu@mail.ru>
2022-04-07 17:14:47 +0300

    implemented book creating functionality

--- js/common.js
+++ js/common.js
@@ -39,9 +39,32 @@ library.forEach(book => {
 
 
 const newBookButton = document.querySelector('#new-book-button')
-const newBookForm = document.querySelector('#new-book-form')
+const newBookForm = document.querySelector('#newBookForm')
+const createBookButton = document.querySelector('#createBookButton')
 
 
 newBookButton.addEventListener('click', () => {
   newBookForm.classList.toggle('hidden')
 })
+
+createBookButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  let name = document.querySelector('#name')
+  let pageCount = document.querySelector('#page-count')
+  let author = document.querySelector('#author')
+  let newBook = new Book(name.value, pageCount.value, author.value)
+  library.push(newBook)
+
+  tableRow = booksTable.insertRow(-1);
+  bookName = tableRow.insertCell([0]);
+  pageCount = tableRow.insertCell([1]);
+  author = tableRow.insertCell([2]);
+  read = tableRow.insertCell([3]);
+
+  bookName.textContent = newBook.name;
+  pageCount.textContent = newBook.pageCount;
+  author.textContent = newBook.author;
+  read.textContent = newBook.read;
+
+})
+
