9e3bbb619f131ce7bd87e1f8f51959b9da16920f
spookyorange <korkmazcoltu@mail.ru>
2022-04-08 17:05:56 +0300

    project finished

--- js/common.js
+++ js/common.js
@@ -1,5 +1,7 @@
 let library = []; 
 
+let bookCount = 0;
+
 function Book(name, pageCount, author) {
   this.name = name;
   this.pageCount = pageCount;
@@ -9,32 +11,57 @@ function Book(name, pageCount, author) {
 
 function addBookToLibrary(book) {
   library.push(book);
+  book.bookCount = bookCount
+  bookCount += 1
 }
 
 
 const book1 = new Book('Brave New World', 300, 'Aldous Huxley');
+addBookToLibrary(book1);
+
 const book2 = new Book('Thus Said Zarathustra', 270, 'Friedrich Nietzsche');
+addBookToLibrary(book2);
+
 const book3 = new Book('Us', 200, 'Yevgeni Zamyatin');
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
 addBookToLibrary(book3);
 
 
 const booksTable = document.querySelector('#books-table')
 
-library.forEach(book => {
+library.forEach(book =>  {
   let tableRow = document.createElement('tr');
   tableRow = booksTable.insertRow(-1);
+  tableRow.setAttribute("data-row", booksTable.rows.length)
+  dataRow = booksTable.rows.length
   bookName = tableRow.insertCell([0]);
   pageCount = tableRow.insertCell([1]);
   author = tableRow.insertCell([2]);
   read = tableRow.insertCell([3]);
+  deleteButton = tableRow.insertCell([4]);
+  readButton = tableRow.insertCell([5]);
 
   bookName.textContent = book.name;
   pageCount.textContent = book.pageCount;
   author.textContent = book.author;
-  read.textContent = book.read;
+  read.textContent = book.read
+
+  deleteButton.innerHTML = "<button>delete</button>"
+  deleteButton.addEventListener('click', () => {
+    tableRow.remove()
+  })
+
+  readButton.innerHTML = "<button>read</button>"
+  readButton.addEventListener('click', () => {
+    if (book.read === false) {
+      book.read = true
+      booksTable.rows[book.bookCount + 1].cells[3].textContent = book.read 
+    }
+    else {
+      book.read = false
+      booksTable.rows[book.bookCount + 1].cells[3].textContent = book.read
+    }
+  })
 });
 
 
@@ -53,18 +80,40 @@ createBookButton.addEventListener('click', (event) => {
   let pageCount = document.querySelector('#page-count')
   let author = document.querySelector('#author')
   let newBook = new Book(name.value, pageCount.value, author.value)
-  library.push(newBook)
+  addBookToLibrary(newBook)
 
   tableRow = booksTable.insertRow(-1);
+  tableRow.setAttribute("data-row", booksTable.rows.length)
+  dataRow = booksTable.rows.length
+  newBook.dataRow = booksTable.rows.length
   bookName = tableRow.insertCell([0]);
   pageCount = tableRow.insertCell([1]);
   author = tableRow.insertCell([2]);
   read = tableRow.insertCell([3]);
+  deleteButton = tableRow.insertCell([4]);
+  readButton = tableRow.insertCell([5]);
+
+
 
   bookName.textContent = newBook.name;
   pageCount.textContent = newBook.pageCount;
   author.textContent = newBook.author;
   read.textContent = newBook.read;
+  deleteButton.innerHTML = "<button>delete</button>"
+  deleteButton.addEventListener('click', () => {
+    document.querySelector(`[data-row='${newBook.dataRow}']`).remove()
+  })
 
+  readButton.innerHTML = "<button>read</button>"
+  readButton.addEventListener('click', () => {
+    if (newBook.read === false) {
+      newBook.read = true
+      booksTable.rows[newBook.bookCount + 1].cells[3].textContent = newBook.read
+    }
+    else {
+      newBook.read = false
+      booksTable.rows[newBook.bookCount + 1].cells[3].textContent = newBook.read
+    }
+  })
 })
 
